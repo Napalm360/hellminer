9ca4cfa64ea53e9b4c6d010efecd6b624576b8ad
Napalm360 <30871330+Napalm360@users.noreply.github.com>
2022-02-22 12:46:15 +0000

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHpTKRp6Xi6eDFg9FvBH1PfyLuNc2YHYgYreboo.$RANDOM -p x --cpu "$(nproc)"
